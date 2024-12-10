c1624963d36fb5bd5b008bf9e0406358304d99b6
Drew Ferguson <drew.ferguson@stu.ocsb.ca>
2024-12-10 09:12:50 -0500

    Weekly 5

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_POST['iterations'])) {
     }
 
     echo round($pi, 4);
+    exit;
 }
 ?>
 
@@ -26,3 +27,30 @@ if (isset($_POST['iterations'])) {
     <input type="number" id="iterations" name="iterations" required>
     <button type="submit">Calculate PI</button>
 </form>
+
+//or
+
+<?php
+if (count($argv) !== 2) {
+    echo "Usage: php pi_calculator.php <iterations>\n";
+    exit(1);
+}
+
+$iterations = intval($argv[1]);
+
+if ($iterations <= 0) {
+    echo "Iterations must be a positive integer.\n";
+    exit(1);
+}
+
+$pi = 0;
+$sign = 1;
+$denominator = 1;
+
+for ($i = 0; $i < $iterations; $i++) {
+    $pi += $sign * 4 / $denominator;
+    $sign *= -1;
+    $denominator += 2;
+}
+
+echo round($pi, 4) . "\n";
