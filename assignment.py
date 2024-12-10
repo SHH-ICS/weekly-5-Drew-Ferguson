014e15b49a046e1f49b2bc1fdf93dcf363b17c95
Drew Ferguson <drew.ferguson@stu.ocsb.ca>
2024-12-10 08:42:27 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,17 @@
+import sys
 
+try:
+    iterations = int(sys.stdin.readline())
+    pi = 0
+    sign = 1
+    denominator = 1
+
+    for _ in range(iterations):
+        pi += sign * 4 / denominator
+        sign *= -1
+        denominator += 2
+
+    print(pi)
+
+except ValueError:
+    print("Invalid input")
